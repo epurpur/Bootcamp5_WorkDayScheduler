738ed469c507c251adc9a9c99f8ace556e4f634c
Erich Purpur <epurpur@gmail.com>
2021-03-23 14:56:19 -0400

    passing data into modal and updating liveTimer()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
 
 
 
-
-// Display current Date and time in Jumbotron
-var currentDateTime = moment().format("MMMM D YYYY H:mm A")
-$("#moment-time").text(currentDateTime);
+// var currentDateTime = moment().format("MMMM D, YYYY hh:mm:ss A")
+// $("#moment-time").text(currentDateTime);
 
 //get hours, minutes from moment.js object
-var hour = moment().hour();
-var minute = moment().minutes();
-console.log(hour, minute);
 
+
+
+
+function liveTime() {
+    // Display current Date and time in Jumbotron
+    // Displays as follows: March 23, 2021 02:52 PM
+    $('#moment-time').html(moment().format('MMMM D YYYY hh:mm A'));
+}
+//calls timer
+setInterval(liveTime, 1000);                                        //updates at interval of 1 minute (1000ms)
 
 
 //loop over each timeslot and get data attribute 'time' from it
@@ -23,23 +28,24 @@ $(".timeslot").each(function() {
     if (time < now) {
         $(this).parent().css({"backgroundColor": "gray"});          // change table row css if condition is true
     } else if (time == now) {
-        $(this).parent().css({"backgroundColor": "red"});          // change table row css if condition is true
+        $(this).parent().css({"backgroundColor": "red"});           // change table row css if condition is true
         //line below the following HTML for edit button to 2nd sibling, which is 3rd column of time table. Button opens 'edit event' modal
-        $(this).siblings().next().append("<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#editEvent' type='submit'>Edit</button>") 
+        $(this).siblings().next().append("<button type='button' class='btn btn-primary editData' data-toggle='modal' data-val='testingdata' data-target='#editEvent' type='submit'>Edit</button>") 
     } else if (time > now) {
         $(this).parent().css({"backgroundColor": "green"});
         //line adds the following HTML for edit button to 2nd sibling, which is 3rd column of time table. Button opens 'edit event' modal
-        $(this).siblings().next().append("<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#editEvent' type='submit'>Edit</button>") //this adds the following HTML for edit button to 2nd sibling, which is 3rd column of time table
+        $(this).siblings().next().append("<button type='button' class='btn btn-primary editData' data-toggle='modal' data-val='testingdata' data-target='#editEvent' type='submit'>Edit</button>") //this adds the following HTML for edit button to 2nd sibling, which is 3rd column of time table
     }
 });
 
 
+//pass data into modal
+$(".editData").click(function() {
+    //traverses DOM to select HTML content of timeslot column of respective 'edit' button.  // Is there a better way?
+    var name = $(this).parent().siblings().prev().html();  
+    //passes HTML content of time slot into text of chosen-time span tag
+    $("#chosen-time").html(name);                           
 
 
+});
 
-// Live Update timer
-// function liveTime() {
-//     $('#moment-time').html(moment().format('MMMM D YYYY H:mm'));
-// }
-// //calls timer
-// setInterval(liveTime, 1000);
