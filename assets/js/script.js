7b8adb93b4b724c6a4ab28623dfddb9d4d23a5a5
Erich Purpur <epurpur@gmail.com>
2021-03-25 10:54:46 -0400

    refactoring css, html, js file and adding comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 
 
+/**
+ * DECLARING FUNCTIONS
+ */
 
 function liveTime() {
     // Display current Date and time in Jumbotron
@@ -7,33 +10,80 @@ function liveTime() {
     $('#moment-time').html(moment().format('MMMM D YYYY hh:mm A'));
 }
 //calls timer
-setInterval(liveTime, 1000);                                        //updates at interval of 1 minute (1000ms)
+setInterval(liveTime, 1000);                                        //updates UI at interval of 1 minute (1000ms)
 
 
 
+function setLocalStorage(objKey, objValue) {
+    
+    //get current data from localStorage. If no data exists, return empty object
+    var dailySchedule = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
 
-//loop over each timeslot and get data attribute 'time' from it
-//depending on time, dynamically colors each row of the table gray, red, green
-//if hour has not passed, add 'edit' button to third column
+    //updates key in dailySchedule and sets equal to objValue. if key doesn't exist yet, creates new key/value pair
+    dailySchedule[objKey] = objValue;
+    
+    //sets updated schedule to localStorage
+    localStorage.setItem('dailySchedule', JSON.stringify(dailySchedule));
+}
+
+
+
+/**
+ * jQuery TO RUN UPON PAGE LOAD
+ */
+
+// upon page load, sets values eventInfo cell in each row of calendar using data from localStorage
+$(document).ready(function() {
+    
+    //get current data from localStorage. If no data exists, return empty object
+    var scheduledItems = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
+    
+    
+    var eventInfoItems = $(".eventInfo")                            //selects all rows of table, 2nd column
+    
+    $.each(eventInfoItems, function(index, object) {                //iterate through each row of schedule
+        var object = $(object);                                     //converts to jQuery object
+        var dataValue = object.attr('value');                       //gets data-value attribute from object
+
+        $.each(scheduledItems, function(key, value) {               //loops through all items in current schedule  
+            if (key == dataValue) {                                 //compares key of local storage items to dataValue
+                object.text(value);                                 // set text of object equal to value for respective rows in schedule
+            }
+        });
+    });
+});
+
+
+
+/** loop over each timeslot and get data attribute 'time' from it
+ *  depending on time, dynamically colors each row of the table gray, red, green
+ *  if hour has not passed, add 'edit' button to third column
+ */ 
 $(".timeslot").each(function() {
     var time = $(this).data("time");                                //gets value of data-time attribute from HTML of each table row
-    // var now = moment().hour();                                   //uses current hour to evaluate colors of table row
-    var now = 13;
+    var now = moment().hour();                                   //uses current hour to evaluate colors of table row
+    // var now = 9;
 
     if (time < now) {
         $(this).parent().css({"backgroundColor": "gray"});          // change table row css if condition is true
     } else if (time == now) {
         $(this).parent().css({"backgroundColor": "red"});           // change table row css if condition is true
-        //line below the following HTML for edit button to 2nd sibling, which is 3rd column of time table. Button opens 'edit event' modal
+        
+        //the following HTML for edit button to 2nd sibling, which is 3rd column of time table. Button opens 'edit event' modal
         $(this).siblings().next().append("<button type='button' class='btn btn-primary editData' data-toggle='modal' data-element='"+time+"' data-target='#editEvent' type='submit'>Edit</button>") 
     } else if (time > now) {
         $(this).parent().css({"backgroundColor": "green"});
-        //line adds the following HTML for edit button to 2nd sibling, which is 3rd column of time table. Button opens 'edit event' modal
-        $(this).siblings().next().append("<button type='button' class='btn btn-primary editData' data-toggle='modal' data-element='"+time+"' data-target='#editEvent' type='submit'>Edit</button>") //this adds the following HTML for edit button to 2nd sibling, which is 3rd column of time table
+        
+        //the following HTML for edit button to 2nd sibling, which is 3rd column of time table. Button opens 'edit event' modal
+        $(this).siblings().next().append("<button type='button' class='btn btn-primary editData' data-toggle='modal' data-element='"+time+"' data-target='#editEvent' type='submit'>Edit</button>")
     }
 });
 
 
+
+/**
+ * BUTTONS
+ */
 
 //pass data into modal
 $(".editData").click(function(event) {
@@ -86,72 +136,12 @@ $('#saveBtn').click(function() {
 
 
 
-function setLocalStorage(objKey, objValue) {
-    
-    //get current data from localStorage. If no data exists, return empty object
-    var dailySchedule = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
-
-    //updates key in dailySchedule and sets equal to objValue. if key doesn't exist yet, creates new key/value pair
-    dailySchedule[objKey] = objValue;
-    
-    //sets updated schedule to localStorage
-    localStorage.setItem('dailySchedule', JSON.stringify(dailySchedule));
-}
-
-
-// upon page load, sets values eventInfo cell in each row of calendar
-$(document).ready(function() {
-    
-    //get current data from localStorage. If no data exists, return empty object
-    var scheduledItems = JSON.parse(localStorage.getItem("dailySchedule") || "{}");
-    
-    //selects all rows of table, 2nd column
-    var eventInfoItems = $(".eventInfo")
-    //iterate through each row of schedule
-    $.each(eventInfoItems, function(i, object) {
-        var object = $(object);   //converts to jQuery object
-        var dataValue = object.attr('value');   
-
-        $.each(scheduledItems, function(key, value) {
-            console.log(key);   
-            if (key == dataValue) {
-                object.text(value);                                 // set text of object equal to value
-            }
-        });
-        
-    });
-
-    // $.each(scheduledItems, function(key, value) {
-    //     console.log(key, value); 
-    // });
-
-
-});
 
 
 
-////////NEED TO SAVE DATA IN LOCAL STORAGE//////////
 
-//thinking about what I want the data structure of local storage to be
-/**
- *  var data = {
- *                  '8'  : 'Free',
- *                  '9'  : 'Staff Meeting',
- *                  '10' : 'Check email',
- *                  '11' : 'Prepare Presentation'
- *              }
- */
 
-// var myObj = {}
 
-// myObj[8] = 'check emails';
-// myObj['9'] = 'staff meeting';
-// myObj['10'] = 'work on project';
-
-// console.log(myObj);
-
-// myObj['8'] = 'prepare presentation';
-// console.log(myObj);
 
 
 
